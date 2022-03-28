7282000371733ca1962998ef720e379ac477149f
Masesesam <101641781+Masesesam@users.noreply.github.com>
2022-03-28 18:05:55 +0300

    Add files via upload

---
+++ _.c perimeter of a rectangle .c
@@ -0,0 +1,12 @@
+/*a program to calculate the perimeter of a rectangle*/
+/*28-4-2022*/
+/*Sam masese*/
+#include<stdio.h>
+int main()
+{
+int a=7;
+int b=5;
+int perimeter=(a+b)*2;
+printf("%d", perimeter);
+return 0;
+}
